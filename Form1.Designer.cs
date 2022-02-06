249fecce4d013b2ce5cc2285a437a176a212f2dc
Vladislav Ivanov <JokeIvanov@yandex.ru>
2022-02-06 12:40:08 +0300

    First program iteration

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace TabMenager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.OpenFilesButton = new System.Windows.Forms.Button();
+            this.SaveFilesButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // OpenFilesButton
+            // 
+            this.OpenFilesButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenFilesButton.Name = "OpenFilesButton";
+            this.OpenFilesButton.Size = new System.Drawing.Size(164, 47);
+            this.OpenFilesButton.TabIndex = 0;
+            this.OpenFilesButton.Text = "Open files";
+            this.OpenFilesButton.UseVisualStyleBackColor = true;
+            this.OpenFilesButton.Click += new System.EventHandler(this.OpenFilesButton_Click);
+            // 
+            // SaveFilesButton
+            // 
+            this.SaveFilesButton.Location = new System.Drawing.Point(206, 12);
+            this.SaveFilesButton.Name = "SaveFilesButton";
+            this.SaveFilesButton.Size = new System.Drawing.Size(164, 47);
+            this.SaveFilesButton.TabIndex = 1;
+            this.SaveFilesButton.Text = "Save files";
+            this.SaveFilesButton.UseVisualStyleBackColor = true;
+            this.SaveFilesButton.Click += new System.EventHandler(this.SaveFilesButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Tg delta";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(111, 98);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "C - capacity";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.SaveFilesButton);
+            this.Controls.Add(this.OpenFilesButton);
+            this.Name = "Form1";
+            this.Text = "Table Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button OpenFilesButton;
+        private System.Windows.Forms.Button SaveFilesButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TabMenager
+{
+    public partial class Form1 : Form
+    {
+        private string[] _fileNames;
+
+        private string _txtFile;
+
+        private List<int> _valuesToSearch = new List<int>
+        {5, 10, 100, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000, 15000000, 20000000};
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkBox1.Checked = true;
+            checkBox2.Checked = false;
+        }
+
+        private void OpenFilesButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "TXT files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int indexColumn = 0;
+
+                if (checkBox1.Checked)
+                {
+                    indexColumn = 2;
+                }
+                else
+                {
+                    indexColumn = 6;
+                }
+                
+                _fileNames = openFileDialog1.FileNames;
+
+                _txtFile = "";
+                label2.Text = "";
+
+
+                for (int f = 0; f < _fileNames.Length; f++)
+                {
+                    StreamReader sr = new StreamReader(_fileNames[f]);
+
+                    List<List<float>> lines = new List<List<float>>();
+
+                    string line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        if (line == "")
+                        {
+                            line = sr.ReadLine();
+                            continue;
+                        }
+
+                        string[] elementsInLine = line.Split(',');
+
+                        List<float> elements = new List<float>();
+
+                        if (elementsInLine[0] != "" && elementsInLine[indexColumn] != "")
+                        {
+                            elements.Add(StringToFloat(elementsInLine[0]));
+                            elements.Add(StringToFloat(elementsInLine[indexColumn]));
+                        }
+                        else
+                        {
+                            label2.Text += "Ошибка в одном из файлов: " + openFileDialog1.SafeFileNames[f] + "\n";
+                        }
+
+                        lines.Add(elements);
+
+                        line = sr.ReadLine();
+                    }
+
+                    List<List<float>> filteredLines = new List<List<float>>();
+
+                    for (int i = 0; i < _valuesToSearch.Count; i++)
+                    {
+                        var closest = float.MaxValue;
+                        var minDifference = float.MaxValue;
+
+                        int indexClosestLine = 0;
+
+                        for (int j = 0; j < lines.Count; j++)
+                        {
+                            var difference = Math.Abs((float)lines[j][0] - _valuesToSearch[i]);
+                            if (minDifference > difference)
+                            {
+                                minDifference = (float)difference;
+                                closest = lines[j][0];
+                                indexClosestLine = j;
+                            }
+                        }
+
+                        filteredLines.Add(lines[indexClosestLine]);
+                    }
+
+                    string onlyName = openFileDialog1.SafeFileNames[f];
+
+                    _txtFile += onlyName.Remove(onlyName.IndexOf('.'), 4);
+
+                    foreach (var filteredLine in filteredLines)
+                    {
+                        _txtFile += "," + filteredLine[1].ToString().Replace(',','.');
+                    }
+
+                    _txtFile += "\n";
+
+                    sr.Close();
+                }
+            }
+        }
+
+        private void SaveFilesButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "TXT files (*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                sw.Write(_txtFile);
+                sw.Close();
+            }
+        }
+
+        private float StringToFloat(string elementStr)
+        {
+            elementStr = elementStr.Replace('.',',');
+
+            return float.Parse(elementStr);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = !checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox2.Checked;
+        }
+    }
+}
