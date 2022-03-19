ed8ecd186377a534d3147151025616ee72a7fa5b
Vladislav Ivanov <JokeIvanov@yandex.ru>
2022-03-19 17:16:12 +0300

    custom frequency search, refactoring

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,13 @@ namespace TabMenager
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.thickness_textBox = new System.Windows.Forms.TextBox();
+            this.thickness_label = new System.Windows.Forms.Label();
+            this.area_label = new System.Windows.Forms.Label();
+            this.area_textBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.valueToSearch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -55,7 +62,7 @@ namespace TabMenager
             // 
             // SaveFilesButton
             // 
-            this.SaveFilesButton.Location = new System.Drawing.Point(206, 12);
+            this.SaveFilesButton.Location = new System.Drawing.Point(223, 12);
             this.SaveFilesButton.Name = "SaveFilesButton";
             this.SaveFilesButton.Size = new System.Drawing.Size(164, 47);
             this.SaveFilesButton.TabIndex = 1;
@@ -66,7 +73,7 @@ namespace TabMenager
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 98);
+            this.checkBox1.Location = new System.Drawing.Point(15, 87);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(65, 17);
             this.checkBox1.TabIndex = 2;
@@ -77,7 +84,7 @@ namespace TabMenager
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(111, 98);
+            this.checkBox2.Location = new System.Drawing.Point(94, 87);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(82, 17);
             this.checkBox2.TabIndex = 3;
@@ -88,16 +95,70 @@ namespace TabMenager
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 169);
+            this.label2.Location = new System.Drawing.Point(18, 432);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 5;
+            // 
+            // thickness_textBox
+            // 
+            this.thickness_textBox.Location = new System.Drawing.Point(76, 123);
+            this.thickness_textBox.Name = "thickness_textBox";
+            this.thickness_textBox.Size = new System.Drawing.Size(183, 20);
+            this.thickness_textBox.TabIndex = 6;
+            // 
+            // thickness_label
+            // 
+            this.thickness_label.AutoSize = true;
+            this.thickness_label.Location = new System.Drawing.Point(12, 126);
+            this.thickness_label.Name = "thickness_label";
+            this.thickness_label.Size = new System.Drawing.Size(59, 13);
+            this.thickness_label.TabIndex = 7;
+            this.thickness_label.Text = "Thickness:";
+            // 
+            // area_label
+            // 
+            this.area_label.AutoSize = true;
+            this.area_label.Location = new System.Drawing.Point(12, 157);
+            this.area_label.Name = "area_label";
+            this.area_label.Size = new System.Drawing.Size(32, 13);
+            this.area_label.TabIndex = 8;
+            this.area_label.Text = "Area:";
+            // 
+            // area_textBox
+            // 
+            this.area_textBox.Location = new System.Drawing.Point(76, 154);
+            this.area_textBox.Name = "area_textBox";
+            this.area_textBox.Size = new System.Drawing.Size(183, 20);
+            this.area_textBox.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.valueToSearch});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 199);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 246);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.UpdateValuesToSearch);
+            // 
+            // valueToSearch
+            // 
+            this.valueToSearch.HeaderText = "Frequency to search";
+            this.valueToSearch.Name = "valueToSearch";
+            this.valueToSearch.Width = 200;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 473);
+            this.ClientSize = new System.Drawing.Size(399, 473);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.area_textBox);
+            this.Controls.Add(this.area_label);
+            this.Controls.Add(this.thickness_label);
+            this.Controls.Add(this.thickness_textBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
@@ -105,6 +166,7 @@ namespace TabMenager
             this.Controls.Add(this.OpenFilesButton);
             this.Name = "Form1";
             this.Text = "Table Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,6 +181,12 @@ namespace TabMenager
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox thickness_textBox;
+        private System.Windows.Forms.Label thickness_label;
+        private System.Windows.Forms.Label area_label;
+        private System.Windows.Forms.TextBox area_textBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueToSearch;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,12 +20,17 @@ namespace TabMenager
         private List<int> _valuesToSearch = new List<int>
         {5, 10, 100, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000, 15000000, 20000000};
 
+        private int indexColumn;
+
         public Form1()
         {
             InitializeComponent();
 
             checkBox1.Checked = true;
             checkBox2.Checked = false;
+
+            UpdateColumnIndex();
+            SetUpStartValueToSearch();
         }
 
         private void OpenFilesButton_Click(object sender, EventArgs e)
@@ -33,99 +38,17 @@ namespace TabMenager
             openFileDialog1.Filter = "TXT files (*.txt)|*.txt";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                int indexColumn = 0;
-
-                if (checkBox1.Checked)
-                {
-                    indexColumn = 2;
-                }
-                else
-                {
-                    indexColumn = 6;
-                }
-                
                 _fileNames = openFileDialog1.FileNames;
 
                 _txtFile = "";
                 label2.Text = "";
-
-
-                for (int f = 0; f < _fileNames.Length; f++)
-                {
-                    StreamReader sr = new StreamReader(_fileNames[f]);
-
-                    List<List<float>> lines = new List<List<float>>();
-
-                    string line = sr.ReadLine();
-
-                    while (line != null)
-                    {
-                        if (line == "")
-                        {
-                            line = sr.ReadLine();
-                            continue;
-                        }
-
-                        string[] elementsInLine = line.Split(',');
-
-                        List<float> elements = new List<float>();
-
-                        if (elementsInLine[0] != "" && elementsInLine[indexColumn] != "")
-                        {
-                            elements.Add(StringToFloat(elementsInLine[0]));
-                            elements.Add(StringToFloat(elementsInLine[indexColumn]));
-                        }
-                        else
-                        {
-                            label2.Text += "Ошибка в одном из файлов: " + openFileDialog1.SafeFileNames[f] + "\n";
-                        }
-
-                        lines.Add(elements);
-
-                        line = sr.ReadLine();
-                    }
-
-                    List<List<float>> filteredLines = new List<List<float>>();
-
-                    for (int i = 0; i < _valuesToSearch.Count; i++)
-                    {
-                        var closest = float.MaxValue;
-                        var minDifference = float.MaxValue;
-
-                        int indexClosestLine = 0;
-
-                        for (int j = 0; j < lines.Count; j++)
-                        {
-                            var difference = Math.Abs((float)lines[j][0] - _valuesToSearch[i]);
-                            if (minDifference > difference)
-                            {
-                                minDifference = (float)difference;
-                                closest = lines[j][0];
-                                indexClosestLine = j;
-                            }
-                        }
-
-                        filteredLines.Add(lines[indexClosestLine]);
-                    }
-
-                    string onlyName = openFileDialog1.SafeFileNames[f];
-
-                    _txtFile += onlyName.Remove(onlyName.IndexOf('.'), 4);
-
-                    foreach (var filteredLine in filteredLines)
-                    {
-                        _txtFile += "," + filteredLine[1].ToString().Replace(',','.');
-                    }
-
-                    _txtFile += "\n";
-
-                    sr.Close();
-                }
             }
         }
 
         private void SaveFilesButton_Click(object sender, EventArgs e)
         {
+            CalculateData();
+
             saveFileDialog1.Filter = "TXT files (*.txt)|*.txt";
 
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
@@ -133,6 +56,82 @@ namespace TabMenager
                 StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
                 sw.Write(_txtFile);
                 sw.Close();
+            }
+        }
+
+        private void CalculateData()
+        {
+            for (int f = 0; f < _fileNames.Length; f++)
+            {
+                StreamReader sr = new StreamReader(_fileNames[f]);
+
+                List<List<float>> lines = new List<List<float>>();
+
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    if (line == "")
+                    {
+                        line = sr.ReadLine();
+                        continue;
+                    }
+
+                    string[] elementsInLine = line.Split(',');
+
+                    List<float> elements = new List<float>();
+
+                    if (elementsInLine[0] != "" && elementsInLine[indexColumn] != "")
+                    {
+                        elements.Add(StringToFloat(elementsInLine[0]));
+                        elements.Add(StringToFloat(elementsInLine[indexColumn]));
+
+                    }
+                    else
+                    {
+                        label2.Text += "Ошибка в файлe: " + openFileDialog1.SafeFileNames[f] + "\n";
+                    }
+
+                    lines.Add(elements);
+
+                    line = sr.ReadLine();
+                }
+
+                List<List<float>> filteredLines = new List<List<float>>();
+
+                for (int i = 0; i < _valuesToSearch.Count; i++)
+                {
+                    var closest = float.MaxValue;
+                    var minDifference = float.MaxValue;
+
+                    int indexClosestLine = 0;
+
+                    for (int j = 0; j < lines.Count; j++)
+                    {
+                        var difference = Math.Abs((float)lines[j][0] - _valuesToSearch[i]);
+                        if (minDifference > difference)
+                        {
+                            minDifference = (float)difference;
+                            closest = lines[j][0];
+                            indexClosestLine = j;
+                        }
+                    }
+
+                    filteredLines.Add(lines[indexClosestLine]);
+                }
+
+                string onlyName = openFileDialog1.SafeFileNames[f];
+
+                _txtFile += onlyName.Remove(onlyName.IndexOf('.'), 4);
+
+                foreach (var filteredLine in filteredLines)
+                {
+                    _txtFile += "," + filteredLine[1].ToString().Replace(',', '.');
+                }
+
+                _txtFile += "\n";
+
+                sr.Close();
             }
         }
 
@@ -146,11 +145,54 @@ namespace TabMenager
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             checkBox2.Checked = !checkBox1.Checked;
+            UpdateColumnIndex();
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Checked = !checkBox2.Checked;
+            UpdateColumnIndex();
+        }
+
+        private void UpdateColumnIndex()
+        {
+            if (checkBox1.Checked)
+            {
+                indexColumn = 2;
+            }
+            else
+            {
+                indexColumn = 6;
+            }
+        }
+
+        private void SetUpStartValueToSearch()
+        {
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < _valuesToSearch.Count; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = _valuesToSearch[i];
+            }
+        }
+
+        private void UpdateValuesToSearch(object sender, DataGridViewCellEventArgs e)
+        {
+            _valuesToSearch.Clear();
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                try
+                {
+                    int value = (int)dataGridView1.Rows[i].Cells[0].Value;
+                    _valuesToSearch.Add(value);
+                }
+                catch
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = "";
+                }
+            }
         }
     }
 }
